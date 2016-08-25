df988615f64f5f3fd03b735cf988ea5c2dd0b662
Andrealiz Afable <dreafable@gmail.com>
2016-08-24 20:38:38 -0700

    complete click box exercise

---
+++ app/assets/javascripts/jackhammer.js
@@ -0,0 +1,14 @@
+var clicks = 0
+
+
+
+function jackhammer() {
+  clicks++;
+    console.log(clicks);
+  if (clicks == 5) {
+    alert("you win!");
+  }
+  else {
+    console.log("you're not clicking enough");
+  }
+}
