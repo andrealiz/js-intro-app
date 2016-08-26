fdf075c815b9f4f244c216b124c638fbec50ced2
Andrealiz Afable <dreafable@gmail.com>
2016-08-25 18:59:15 -0700

    add function change color of box every click

--- app/assets/javascripts/jackhammer.js
+++ app/assets/javascripts/jackhammer.js
@@ -1,11 +1,20 @@
 var clicks = 0;
+var box = document.getElementById("box-game");
 
-function jackhammer() {
+function jackhammer(box) {
   clicks++;
     console.log(clicks);
-  if (clicks === 5) {
-    alert("you win!");
+  if (clicks === 1) {
+    box.style["background-color"] = "green";
+  } else if (clicks === 2) {
+    box.style["background-color"] = "yellow";
+  } else if (clicks === 3) {
+    box.style["background-color"] = "orange";
+  } else if (clicks === 4) {
+    box.style["background-color"] = "red";
+  } else if (clicks === 5) {
+    box.style["background-color"] = "purple";
   } else {
-    console.log("you're not clicking enough");
+    console.log("Stop clicking");
   }
 }
